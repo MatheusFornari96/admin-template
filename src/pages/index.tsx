85cd55f554634f3d2a03a43a6bca7eb435eaedfc
Matheus Fornari <71535472+MatheusFornari96@users.noreply.github.com>
2022-08-02 09:29:24 -0300

    projeto criado

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-gradient-to-r from-green-500 to-blue-500">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
