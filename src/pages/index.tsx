3cfaf5de1fb945f6ec3b1c371ecab46b1a6603c2
Matheus Fornari <71535472+MatheusFornari96@users.noreply.github.com>
2022-08-05 08:06:35 -0300

    create page auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/template/Layout'
 export default function Home() {
   return (
     <Layout titulo="Página Inicial" subtitulo="Estamos construindo um template Admin!" >
-      <h3>Conteudo</h3>
+      <h3>Conteúdo</h3>
     </Layout>
   )
 }
