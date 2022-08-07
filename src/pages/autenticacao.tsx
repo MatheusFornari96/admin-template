d51babac340293b4d27838f094bd06d753604aad
Matheus Fornari <matheusfornari@hotmail.com>
2022-08-07 14:22:41 -0300

    a179

--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import AuthInput from "../components/auth/AuthInput";
+import { GoogleIcon } from "../components/icons/index";
 
 export default function Autenticacao() {
 
@@ -7,23 +8,54 @@ export default function Autenticacao() {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
 
+  function submeter() {
+    if (modo === 'login') {
+      console.log('login');
+    } else {
+      console.log('cadastrar');
+    }
+  }
+
   return (
-    <div className="flex flex-col">
-      <h1>Autenticação</h1>
-      <AuthInput
-        label="Email"
-        tipo="email"
-        valor={email}
-        valorMudou={setEmail}
-        required
-      />
-      <AuthInput
-        label="Senha"
-        tipo="password"
-        valor={senha}
-        valorMudou={setSenha}
-        required
-      />
+    <div className="flex flex-col h-screen items-center justify-center">
+      <div className="w-1/2">
+        <h1 className={`
+      text-xl font-bold mb-5
+      `}>
+          {modo === 'login' ? 'Entre com a sua conta' : 'Cadastre-se na Plataforma'}
+        </h1>
+        <AuthInput
+          label="Email"
+          tipo="email"
+          valor={email}
+          valorMudou={setEmail}
+          required
+        />
+        <AuthInput
+          label="Senha"
+          tipo="password"
+          valor={senha}
+          valorMudou={setSenha}
+          required
+        />
+
+        <button onClick={submeter} className={`
+          w-full bg-blue-500 hover:bg-blue-400
+          text-white rounded-lg px-4 py-3 mt-6
+      `}>
+          {modo === 'login' ? 'Entrar' : 'Cadastrar'}
+        </button>
+        <hr className="my-6 border-gray-300 w-full" />
+
+        <button onClick={submeter} className={`
+          w-full bg-red-500 hover:bg-red-400
+          text-white rounded-lg px-4 py-3 mt-6
+          flex flex-row justify-center items-center
+      `}>
+          {GoogleIcon}
+          <span className="ml-2">Entrar com Google</span>
+        </button>
+      </div>
     </div>
   )
 }
